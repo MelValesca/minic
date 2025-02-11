8582103882ef8cbc55b04adf70d083816742a9d1
Jean Privat <jean@pryen.org>
2025-02-11 14:24:12 -0500

    improve build script

--- minic/build.sh
+++ minic/build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
+
+if [ ! -e sablecc-4-beta.2 ]; then
+	wget http://downloads.sourceforge.net/sablecc/sablecc-4-beta.2.zip
+	unzip sablecc-4-beta.2.zip
+fi
+
+if find src/ ./minic.sablecc -type f -printf '%T@ %p\n' | sort | tail -n 1 | grep -q minic.sablecc; then
+	java -jar sablecc-4-beta.2/lib/sablecc.jar minic.sablecc -p minic -d src
+fi
+
 mkdir -p build
-if find src/ build/ -type f -printf '%T@ %p\n' | sort | tail -n 1 | grep java; then
-#java=$(ls -tr src/*.java src/*/*.java | tail -n 1)
-#class=$(ls -tr build/*.class | tail -n 1)
-#tj=$(stat -c%Y "$java")
-#tc=$(stat -c%Y "$class")
-#if [ "$tj" -gt "$tc" ]; then
+if find src/ build/ -type f -printf '%T@ %p\n' | sort | tail -n 1 | grep -q java; then
+	if [ "$#" = 0 ]; then
+		set src/minic/*.java
+	fi
 	javac -d build --source-path src "$@"
 fi
